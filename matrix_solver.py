2a44b44b741fa66500aaad2d229101321d5ef16b
3370sohail <3370sohail@gmail.com>
2017-06-09 06:00:32 -0400

    add row mutiplication and fractions
    added row mutlipication and fraction repersentation

--- matrix_solver.py
+++ matrix_solver.py
@@ -1,6 +1,6 @@
 """ matrix solver + latex printer
 """
-
+from fractions import *
 
 class Matrix:
     """ matrix class
@@ -26,27 +26,47 @@ class Matrix:
 
         @rtype: Matrix
         """
+        x = 1
 
+    def mutlipy_row(self, row_num, multiplier):
+        """
+        scaler multiply a row
+        @rtype: None | Print
+        >>> a = Fraction(2,1)
+        >>> b = Fraction(21,13)
+        >>> print(a.numerator%a.denominator , type(a+a), int(1))
+        """
+        for i in range(len(self.arr[row_num-1])):
+            self.arr[row_num-1][i] = self.arr[row_num-1][i] * multiplier
 
+        if self.latex:
+            opertation = '\\xrightarrow[]{'
+            if (isinstance(multiplier, int) or
+                multiplier.numerator % multiplier.denominator == 0):
+                opertation += str(int(multiplier)) + 'r_' + str(row_num) + '}'
+            else:
+                opertation += ('\\frac{' + str(multiplier.numerator) + '}{' +
+                               str(multiplier.denominator) + '}' +
+                               'r_' + str(row_num) + '}')
+            print(opertation)
+            print(self)
 
     def __str__(self):
         """
         latex string repsentation of a matirx
 
-        \left[
-        \begin{array}{ccc|c}
-        -1 &  3 & 1 & 1 \\
-        3 &  1 & 1 & 2\\
-        0 &  2 & 1 & 4 \\
-        1 &  0 & 1 & 0\\
-        \end{array}
-        \right]
-
         @rtype: str
 
         >>> c = Matrix([[1, 2], [3, 4]], [[1],[2]], True )
+        >>> c.mutlipy_row(1, Fraction(1,2))
         >>> print(c)
-        ''
+        \left[
+        \begin{array}{cc|c}
+        1 & 2 & 1  \\
+        3 & 4 & 2  \\
+        \end{array}
+        \right]
+        }{}
         """
         output = '\\left[ \n\\begin{array}{' + self.cols*'c'
         if self.c_cols >= 1:
@@ -55,17 +75,25 @@ class Matrix:
 
         for index, row in enumerate(self.arr):
             for i in row:
-                output += str(i) + ' & '
+                if isinstance(i, int) or i.numerator % i.denominator == 0:
+                    output += str(int(i)) + ' & '
+                else:
+                    output += ('\\frac{' + str(i.numerator) + '}{' +
+                               str(i.denominator) + '}' + ' & ')
             if self.c_cols >= 1:
                 for j in self.augment[index]:
-                    output += str(j) + ' & '
+                    if isinstance(j, int) or j.numerator % j.denominator == 0:
+                        output += str(int(j)) + ' & '
+                    else:
+                        output += ('\\frac{' + str(j.numerator) + '}{' +
+                                   str(j.denominator) + ' & ')
             output = output[:-2] + ' \\\\  \n'
         output += '\\end{array} \n\\right]'
         return output
 
-def REF(arr, augment):
+#def REF(arr, augment):
 
-def RREF():
+#def RREF():
 
 
 
